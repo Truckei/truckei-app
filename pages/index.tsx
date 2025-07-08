03b4ec73d57e580dcefad7e50e4510bd14051ae3
Truckei <truckeidemarca@gmail.com>
2025-07-08 20:59:49 -0300

    Create index.tsx

---
+++ pages/index.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ padding: "2rem", textAlign: "center" }}>
+      <h1>Bem-vindo à Truckei!</h1>
+      <p>Seu comparador de caminhões 0km.</p>
+    </div>
+  );
+}
